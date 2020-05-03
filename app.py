f5f6fee3bdb827fa064699f29411e588c6817346
Robert <rr.rijksen@gmail.com>
2020-05-03 16:26:39 +0200

    update

--- app.py
+++ app.py
@@ -3,10 +3,28 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return render_template("testing_stuff.html")
+@app.route('/', methods=['get', 'post'])
+def homepage():
+    """Deze functie runt de app
+    :return: homepagina met informatie over de website
+    """
+    return render_template('home.html')
+
+
+@app.route('/textminer', methods=['get', 'post'])
+def textminer():
+    return render_template("text_miner.html")
+
+
+@app.route('/resultaat', methods=['get', 'post'])
+def result():
+    return render_template("resultaat.html")
+
+
+@app.route('/parameters', methods=['get', 'post'])
+def parameters():
+    return render_template("parameters.html")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
