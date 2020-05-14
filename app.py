e4e93c19e2bd6a293e490c3c6ca0d36d09b342f8
NI-S <noah.i.ching.scheffer@gmail.com>
2020-05-14 12:30:52 +0200

    eerste 10 resultaten worden laten zien (als er een gen of iets anders wordt ingevoerd). Parameter knop werkt nog niet en ook het dropdown werkt nu nog niet

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from Bio import Entrez
 
 app = Flask(__name__)
 
@@ -11,15 +12,58 @@ def homepage():
     return render_template('home.html')
 
 
-@app.route('/textminer', methods=['get', 'post'])
+@app.route('/textminer')
 def textminer():
+    return render_template("text_miner.html")
+
+@app.route('/textminer', methods=['post'])
+def textmined():
     return render_template("text_miner.html")
 
 
 @app.route('/resultaat', methods=['get', 'post'])
 def result():
-    return render_template("resultaat.html")
+    #print(request)
+    zoekwoord = request.form["woord"]
+    gezocht, aantal = zoeken(zoekwoord)
+    id_lijst = gezocht['IdList']
+    details = details_ophalen(id_lijst)
+    samenvatting = verkrijg_titel(details, zoekwoord)
+    return render_template("resultaat.html") + samenvatting
 
+def zoeken(zoekwoord):
+    Entrez.email = 'example@mail.com'
+    handle = Entrez.esearch(db='pubmed',
+                            sort='relevance',
+                            retmode='xml',
+                            retmax=10,
+                            term=zoekwoord)
+    results = Entrez.read(handle)
+    aantal = results['Count']
+    return results, aantal
+
+def details_ophalen(id_lijst):
+    ids = ','.join(id_lijst)
+    Entrez.email = 'example@mail.com'
+    handle = Entrez.efetch(db='pubmed',
+                           retmode='xml',
+                           id=ids)
+    results = Entrez.read(handle)
+    return results
+
+def verkrijg_titel(publicaties, zoekwoord):
+    overzicht = "<table><tr><th>zoekwoord</th><th>publicatiedatum</th><th>artikeltitel pubmed</th></tr>"
+    for i, paper in enumerate(publicaties['PubmedArticle']):
+        try:
+            year = int(paper['MedlineCitation']['Article']['Journal']['JournalIssue']['PubDate']['Year'])
+            artikel = (paper['MedlineCitation']['Article']["ArticleTitle"])
+            overzicht = overzicht + "<tr><td>" + zoekwoord +"</td>" + "<td>" + str(year) + "</td><td>" + str(artikel)\
+                        + "</td></tr>"
+        except:
+            pass
+    print(overzicht)
+    overzicht = overzicht + "</table>"
+    return overzicht
 
 @app.route('/parameters', methods=['get', 'post'])
 def parameters():
